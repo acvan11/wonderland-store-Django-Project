90ba64432885bd29a4de6099bc6a2886f4074425
Sky <dungchogithub@gmail.com>
2018-10-08 13:24:44 -0700

    admin manage

--- project4_app/admin.py
+++ project4_app/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+	# show column description, price, sales_price on admin sites
+	list_display = ["__str__", "description", "price", "sales_price"]
+	# have a search bar that search the name or description
+	search_fields = ["name", "description"]
+	# have a filter bar at the right side
+	list_filter = ["price", "sales_price"]
+	# we could edit the sales_price and price 
+	list_editable = ["sales_price", "price"]
+	class Meta:
+		model = Product
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
