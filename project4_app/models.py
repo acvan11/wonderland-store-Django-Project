e213f34c244fc9a95aead8c9993fc501a9e4c70f
Sky <dungchogithub@gmail.com>
2018-10-09 05:08:59 -0700

    add the signal on the models

--- project4_app/models.py
+++ project4_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signal import pre_save, post_save
 
 # Create your models here.
 class Product(models.Model):
@@ -9,3 +10,9 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+
+def product_pre_save_reciever(sender, instance, *args, **kwargs):
+	print sender
+	print instance
+
+pre_save.connect(product_pre_save_reciever, sender=Product)
