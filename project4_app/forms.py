53c1e2afb0b3f0ace3c282189d825dc847554e9b
Sky <dungchogithub@gmail.com>
2018-10-10 11:01:52 -0700

    update forms.py

--- project4_app/forms.py
+++ project4_app/forms.py
@@ -1,36 +1,7 @@
 from django import forms
 from .models import Product
 
-class ProductAddForm(forms.Form):
-	name = forms.CharField(label="Product Name", widget=forms.TextInput(
-		attrs={
-			"class": "custom-class",
-			"placeholder": "Product Name",
-		}))
-	description = forms.CharField(widget=forms.Textarea(
-		attrs ={
-			"class": "my-custom-class",
-			"placeholder": "Product Description",
-			"some-attr": "this",
-		}))
-	price = forms.FloatField()
-
-class ProductModelForm(forms.ModelForm):
+class ProductForm(forms.ModelForm):
 	class Meta:
 		model = Product
-		fields = [
-		"name",
-		"description",
-		"price"
-		]
-		widgets = {
-		"description": forms.Textarea(
-			attrs={
-			"placeholder": "Product Description"
-			}
-			),
-		"name": forms.TextInput(
-			attrs={
-			"placeholder": "Product Name"
-			})
-		}
+		fields = ('name', 'description', 'price', 'sales_price')
