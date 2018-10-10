8c9f2ddf3fc162ca1b60d8f24408a8068d428bc9
Sky <dungchogithub@gmail.com>
2018-10-10 11:16:37 -0700

    edit base page

--- project4_app/views.py
+++ project4_app/views.py
@@ -23,15 +23,15 @@ def product_edit(request, pk):
 		form = ProductForm(instance=product)
 		return render(request, 'product_new.html', {'form': form})
 
-
-
-# list of all products
-def list_product(request):
-	products = Product.objects.all()
-	return render(request, "list_products.html", {"products" : products})
-
-
-
+def product_create(request):
+	if request.method == 'POST':
+		form = ProductForm(request.POST)
+		if form.is_valid():
+			product = form.save()
+			return redirect('product_detail', pk=product.pk)
+	else:
+		form = ProductForm()
+		return render(request, 'product_new.html', {'form': form})
 
 
 
