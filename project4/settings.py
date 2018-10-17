86866c1530ccf27293a21d495168451744607b43
Sky <dungchogithub@gmail.com>
2018-10-17 14:48:12 -0700

    make it better

--- project4/settings.py
+++ project4/settings.py
@@ -119,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'project4_app/static'),
+    os.path.join(BASE_DIR, 'project4_app'),
+)
